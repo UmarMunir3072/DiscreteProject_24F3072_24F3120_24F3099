d45e9b986e5876d56163f6060a8426fa12adc267
UmarMunir3072 <f243072@cfd.nu.edu.pk>
2025-11-25 23:02:10 +0500

    Final PUSH

--- UnidiscEngine.cpp
+++ UnidiscEngine.cpp
@@ -497,17 +497,251 @@ void UnidiscEngine::analyzeRelations()
 }
 void UnidiscEngine::analyzeFunctions()
 {
-    cout << "--- Module 7: Functions ---" << endl;
-    bool valid = true;
+    cout << "--- Module 7: Functions, Composition & Inverse ---" << endl;
+
+    if (studentCount == 0 || courseCount == 0)
+    {
+        cout << "Error: Insufficient data for analysis." << endl;
+        return;
+    }
+
+    cout << endl;
+    cout << "[Analysis 1] Mapping: Students -> Courses" << endl;
+
+    bool scIsFunction = true;
+    for (int i = 0; i < studentCount; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < enrollCount; j++)
+        {
+            if (enrollments[j].elementA == students[i].id)
+            {
+                count = count + 1;
+            }
+        }
+        if (count != 1)
+        {
+            scIsFunction = false;
+        }
+    }
+
+    if (!scIsFunction)
+    {
+        cout << "Result: NOT a Function (Some students take 0 or >1 courses)." << endl;
+    }
+    else
+    {
+        cout << "Result: Valid Function." << endl;
+
+        bool isInjective = true;
+        for (int i = 0; i < courseCount; i++)
+        {
+            int mapped = 0;
+            for (int j = 0; j < enrollCount; j++)
+            {
+                if (enrollments[j].elementB == courses[i].code)
+                {
+                    mapped = mapped + 1;
+                }
+            }
+            if (mapped > 1)
+            {
+                isInjective = false;
+            }
+        }
+
+        bool isSurjective = true;
+        for (int i = 0; i < courseCount; i++)
+        {
+            int mapped = 0;
+            for (int j = 0; j < enrollCount; j++)
+            {
+                if (enrollments[j].elementB == courses[i].code)
+                {
+                    mapped = mapped + 1;
+                }
+            }
+            if (mapped == 0)
+            {
+                isSurjective = false;
+            }
+        }
+
+        if (isInjective)
+        {
+            cout << " - Injective: YES" << endl;
+        }
+        else
+        {
+            cout << " - Injective: NO" << endl;
+        }
+
+        if (isSurjective)
+        {
+            cout << " - Surjective: YES" << endl;
+        }
+        else
+        {
+            cout << " - Surjective: NO" << endl;
+        }
+
+        if (isInjective && isSurjective)
+        {
+            cout << " - Bijective: YES" << endl;
+        }
+        else
+        {
+            cout << " - Bijective: NO" << endl;
+        }
+    }
+
+    cout << endl;
+    cout << "[Analysis 2] Mapping: Courses -> Faculty" << endl;
+
+    bool cfIsFunction = true;
     for (int i = 0; i < courseCount; i++)
     {
-        int c = 0;
+        int count = 0;
         for (int j = 0; j < facAssignCount; j++)
-            if (facultyAssignments[j].elementA == courses[i].code) c++;
-        if (c != 1) valid = false;
+        {
+            if (facultyAssignments[j].elementA == courses[i].code)
+            {
+                count = count + 1;
+            }
+        }
+        if (count != 1)
+        {
+            cfIsFunction = false;
+        }
     }
-    if (valid) cout << "Faculty Assignment is a valid Function." << endl;
-    else cout << "Faculty Assignment is NOT a function." << endl;
+
+    if (!cfIsFunction)
+    {
+        cout << "Result: NOT a Function." << endl;
+    }
+    else
+    {
+        cout << "Result: Valid Function." << endl;
+
+        bool isInjective = true;
+        for (int i = 0; i < facultyCount; i++)
+        {
+            int mapped = 0;
+            for (int j = 0; j < facAssignCount; j++)
+            {
+                if (facultyAssignments[j].elementB == faculties[i].id)
+                {
+                    mapped = mapped + 1;
+                }
+            }
+            if (mapped > 1)
+            {
+                isInjective = false;
+            }
+        }
+
+        bool isSurjective = true;
+        for (int i = 0; i < facultyCount; i++)
+        {
+            int mapped = 0;
+            for (int j = 0; j < facAssignCount; j++)
+            {
+                if (facultyAssignments[j].elementB == faculties[i].id)
+                {
+                    mapped = mapped + 1;
+                }
+            }
+            if (mapped == 0)
+            {
+                isSurjective = false;
+            }
+        }
+
+        if (isInjective)
+        {
+            cout << " - Injective: YES" << endl;
+        }
+        else
+        {
+            cout << " - Injective: NO" << endl;
+        }
+
+        if (isSurjective)
+        {
+            cout << " - Surjective: YES" << endl;
+        }
+        else
+        {
+            cout << " - Surjective: NO" << endl;
+        }
+
+        if (isInjective && isSurjective)
+        {
+            cout << " - Bijective: YES" << endl;
+            cout << "   [Inverse Map Generated: Faculty -> Course]" << endl;
+            cout << "   { ";
+            for (int k = 0; k < facAssignCount; k++)
+            {
+                cout << "(" << facultyAssignments[k].elementB << "->" << facultyAssignments[k].elementA << ") ";
+            }
+            cout << "}" << endl;
+        }
+        else
+        {
+            cout << " - Bijective: NO (Inverse not possible)" << endl;
+        }
+    }
+
+    cout << endl;
+    cout << "[Analysis 3] Composition: Student -> Faculty" << endl;
+    cout << "Logic: (Student -> Course) o (Course -> Faculty)" << endl;
+    cout << "Pairs: { ";
+    for (int i = 0; i < enrollCount; i++)
+    {
+        string s = enrollments[i].elementA;
+        string c = enrollments[i].elementB;
+
+        string f = "None";
+        for (int j = 0; j < facAssignCount; j++)
+        {
+            if (facultyAssignments[j].elementA == c)
+            {
+                f = facultyAssignments[j].elementB;
+            }
+        }
+
+        if (f != "None")
+        {
+            cout << "(" << s << " -> " << f << ") ";
+        }
+    }
+    cout << "}" << endl;
+
+    cout << endl;
+    cout << "[Analysis 4] Mapping: Faculty -> Rooms" << endl;
+    cout << "Logic: Faculty -> Course -> Room" << endl;
+
+    for (int i = 0; i < facultyCount; i++)
+    {
+        string fID = faculties[i].id;
+        cout << "  Faculty " << fID << " teaches in: { ";
+
+        for (int j = 0; j < facAssignCount; j++)
+        {
+            if (facultyAssignments[j].elementB == fID)
+            {
+                string c = facultyAssignments[j].elementA;
+                for (int k = 0; k < roomAssignCount; k++)
+                {
+                    if (roomAssignments[k].elementA == c)
+                    {
+                        cout << roomAssignments[k].elementB << " ";
+                    }
+                }
+            }
+        }
+        cout << "}" << endl;
+    }
 }
 
 void UnidiscEngine::provePrereqChain(string course)
@@ -686,7 +920,7 @@ void UnidiscEngine::runDummyMode()
 {
     cout << " RUNNING MODULE 12: DUMMY TEST CASES & SUITE " << endl;
 
-    cout << " Populating Dummy Data (Module 5)..." << endl;
+    cout << " Populating Dummy Data ..." << endl;
     addStudent("S1", "Umar", "CS", 3);
     addStudent("S2", "Ali", "SE", 2);
     addStudent("S3", "Rayan", "CS", 3);
@@ -698,17 +932,17 @@ void UnidiscEngine::runDummyMode()
     addFaculty("1", "Dr. Qamar");
     addRoom("R1", "Lab 1");
 
-    cout << endl << " Testing Prerequisities (Module 6)..." << endl;
+    cout << endl << " Testing Prerequisities" << endl;
     addPrerequisite("CS101", "CS102");
     addPrerequisite("CS102", "CS201");
 
-    cout << endl << " Testing Enrollment (Module 6)..." << endl;
+    cout << endl << " Testing Enrollment" << endl;
     enrollStudent("S1", "CS201");
     enrollStudent("S2", "CS102");
     enrollStudent("S3", "CS201");
 	enrollStudent("S2", "CS101");
 
-    cout << endl << " Testing Assignments (Module 7)..." << endl;
+    cout << endl << " Testing Assignments" << endl;
     assignFaculty("CS101", "1");
     assignFaculty("CS102", "1");
     assignFaculty("CS201", "1");
